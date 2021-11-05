f7e85ba1bc6fde634ea879566634c54082d516e1
bibi <bibi6666667@kakao.com>
2021-11-05 16:33:23 +0900

    아스키 코드

---
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/p11654.swift
@@ -0,0 +1,13 @@
+//
+//  p11654.swift
+//  cocoa-friday-algorithm
+//
+//  Created by Bibi on 2021/11/05.
+//
+
+import Foundation
+
+func runP11654() {
+    let input = readLine() ?? ""
+    print(Character(input).asciiValue ?? "")
+}
